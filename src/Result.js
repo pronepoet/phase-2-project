0a59559b0ea42a1f561623fe362e98eaf3d223b5
pronepoet <ochiengphillip84@gmail.com>
2022-10-06 03:19:48 +0300

    Added all the files

---
+++ src/Result.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Result(){
+    return(
+        <div>
+            <tr>
+                <td>{date}</td>
+                <td>{description}</td>
+                <td>{}</td>
+                <td></td>
+            </tr>
+        </div>
+    )
+}
---
+++ src/ResultContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+import AddStudent from "./AddStudent";
+import { useEffect, useState } from "react";
+function ResultContainer(){
+return (
+    <div>
+        
+    </div>
+)
+}
+export default ResultContainer
